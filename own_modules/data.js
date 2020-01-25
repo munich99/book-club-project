84be76c684158a7a6eb8212b8adc048545d2e789
munich99 <regerstrasse@icloud.com>
2020-01-25 13:06:10 +0100

    auth server pretesting-get

---
+++ own_modules/data.js
@@ -0,0 +1,6 @@
+'use strict';
+module.exports= [
+    { "id": "tt0110357", "name": "The Lion King", "genre": "animation"},
+    { "id": "tt0068646", "name": "The Godfather", "genre": "crime"},
+    { "id": "tt0468569", "name": "The Dark Knight", "genre": "action"},
+];
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 'use strict';
-
 // testing
 console.log("Hello World!");
 
@@ -9,16 +8,33 @@ const server = express();
 // route handler
 server.get('/json', (req,res)=>{
     res.json({message:"hallo world!!"})    
-})
+});
 server.get('/',(req,res)=>{
     res.sendFile(__dirname + "/index.html");
 });
 
-// Start server
+// start server
 const port = 4000;
 server.listen(port, ()=>{
-    console.log(`server listen at port ${port}`);
+    console.log(`server listen at port ${port}`);    
+});
+
+// own apps
+let data = require('./own_modules/data');
+server.get('/items', (req,res)=>{
+    res.json(data);        
+});
+
+server.get('/items/:id', (req,res)=>{
+    const itemId = req.params.id;
+    const item = data.find(_item =>_item.id === itemId);
+
+    if(item)res.json(item)
+    else res.json({massage:"id nicht vorhanden"})
     
-})
+
+});
+
+
 
 
