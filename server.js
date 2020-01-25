c64b11ba30636c461b6826a7fd0d9b79d2bf9a72
munich99 <regerstrasse@icloud.com>
2020-01-25 12:35:30 +0100

    auth server pretesting

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 'use strict';
+
+// testing
+console.log("Hello World!");
+
+const express = require('express');
+const server = express();
+
+// route handler
+server.get('/json', (req,res)=>{
+    res.json({message:"hallo world!!"})    
+})
+server.get('/',(req,res)=>{
+    res.sendFile(__dirname + "/index.html");
+});
+
+// Start server
+const port = 4000;
+server.listen(port, ()=>{
+    console.log(`server listen at port ${port}`);
+    
+})
+
+
