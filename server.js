c07eb1c21b0288b1c9ce67583772d837e0d2996e
munich99 <regerstrasse@icloud.com>
2020-01-25 16:53:50 +0100

    auth server pretesting-post

--- server.js
+++ server.js
@@ -3,37 +3,55 @@
 console.log("Hello World!");
 
 const express = require('express');
-const server = express();
+const auth = express();
 
 // route handler
-server.get('/json', (req,res)=>{
+auth.get('/json', (req,res)=>{
     res.json({message:"hallo world!!"})    
 });
-server.get('/',(req,res)=>{
+auth.get('/',(req,res)=>{
     res.sendFile(__dirname + "/index.html");
 });
 
-// start server
+// start auth
 const port = 4000;
-server.listen(port, ()=>{
-    console.log(`server listen at port ${port}`);    
+auth.listen(port, ()=>{
+    console.log(`auth listen at port ${port}`);    
 });
 
 // own apps
 let data = require('./own_modules/data');
-server.get('/items', (req,res)=>{
+auth.get('/items', (req,res)=>{
     res.json(data);        
 });
 
-server.get('/items/:id', (req,res)=>{
+auth.get('/items/:id', (req,res)=>{
     const itemId = req.params.id;
     const item = data.find(_item =>_item.id === itemId);
 
     if(item)res.json(item)
-    else res.json({massage:"id nicht vorhanden"})
-    
-
+    else res.json({massage:"id nicht vorhanden"})  
 });
+
+// post auth
+/* for #postman# dont forget
+ header-> content-type:application/json
+ body -> {"key":"value"}
+ */
+
+const body_parser = require('body-parser');
+auth.use(body_parser.json());
+
+auth.post("/auth", (req, res) => {
+    console.log(req.body.id,"id")
+    let ergebnis = {
+        id: req.body.id,
+        ff:  req.body.ff
+    };
+
+    res.send ( JSON.stringify ( ergebnis ) );
+ });
+
 
 
 
