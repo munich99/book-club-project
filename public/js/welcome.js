f216e330cffa7f7443078b7f790af4bc705d8d08
munich99 <ask@filizanka.net>
2020-02-03 09:12:08 +0100

    preaparing welcome.js

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -1,1 +1,7 @@
-'use strict';
+'use strict';    
+console.log(localStorage.getItem("user"));
+console.log(localStorage.getItem("token"));  
+
+document.addEventListener ( 'DOMContentLoaded', () => {
+    document.getElementById("willkommen").innerHTML = localStorage.getItem("user");
+});
