d47c7a0858f7a4af380558d19d972d4b3e815c63
munich99 <ask@filizanka.net>
2020-02-03 15:17:30 +0100

    appendChild inside appendChild

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -3,13 +3,27 @@ console.log(localStorage.getItem("user"));
 console.log(localStorage.getItem("token"));  
 
 let obj=JSON.parse(localStorage.getItem("books"));
-Object.keys(obj).forEach(key=>{
-
-	console.log(`${key} : ${obj[key]}`);
-
- });
  
 
 document.addEventListener ( 'DOMContentLoaded', () => {
-    document.getElementById("willkommen").innerHTML = localStorage.getItem("user");
+
+	document.getElementById("willkommen").innerHTML = localStorage.getItem("user");
+
+		
+	Object.keys(obj).forEach(key=>{
+		let node = document.createElement("li");                
+		let textnode = document.createTextNode(`${obj[key]}`); 
+		node.appendChild(textnode);
+
+		let newElement = document.createElement("span");
+		let textnode2 = document.createTextNode(`${key}`); 
+		newElement.appendChild(textnode2);
+
+		node.appendChild(newElement);	
+		document.getElementById("deine-buecher-liste").appendChild(node);
+
+
+		console.log(`${key} : ${obj[key]}`);
+	 });
+
 });
