1074131be7d32573ab5d3c9d71aefe53e75cd82d
munich99 <regerstrasse@icloud.com>
2020-02-10 21:03:47 +0100

    serch for title and username

--- server.js
+++ server.js
@@ -177,88 +177,20 @@ app.post("/welcome/a/:neighbours", (req, res) => {
 
     couch.get(dbName, viewUrl).
         then(({data, headers, status}) => { 
-            let array2      = data.rows; 
+            let alleEintraege      = data.rows; 
             let findsearch  = [], findsearchUnique, fn, obj; 
-
-            let array3;
             
-            array2.forEach(ele1 => {
-                if(userbook.user !== ele1.value.firstname){
-
-
-                    array3 = ele1.id;
-                   console.log(array3);
-
-                
-
-                   
-
-
-                   
-
-
-
-
-                }
-            })
             
-            /*
-            for(let i= 0; i< (array2.length); i++) {  
-
-                // console.log(array2[i].value.firstname,"name");
-
-                
-
-
-                if(array2[i].value.books){
-                    
-                    obj = array2[i].value.books;
-                    fn  = userbook.searchtheme; 
-                    obj.forEach(element => {
-                        
-                       // console.log(element.title,"+++++++++++++");
-
-                        switch (userbook.searchtheme) {
-                            case 'author' :  // for searching author/(the key)
-                                if( array2[i].value.autor === userbook.searchcontent && array2[i].value.firstname != userbook.user ) {  
-                                    findsearch.push(array2[i].value.firstname)                         
-                                }
-                               break;
-                            case 'title' :  // for searching title/(the value)
-                                if( element.title ==  userbook.searchcontent && userbook.user !== (array2[i].value.firstname) ) {  
-                                    console.log("title##################################");
-                                    findsearch.push(array2[i].value.firstname)     
-                                    
-                                                        
-                                }                        
-                               break;
-                            case 'genre' :  // for searching title/(the value)
-                                if( array2[i].value.genre ===  userbook.searchcontent && array2[i].value.firstname != userbook.user ) {  
-                                    findsearch.push(array2[i].value.firstname)                         
-                                }                        
-                                break;
-                            default:
-                                console.log("nix thema");
-                         }
-
-
-                        
-
-
-                        if(userbook.searchcontent === element[fn] && userbook.user !== (array2[i].value.firstname) ) {
-                        // console.log("gefunden bei" + array2[i].value.firstname); 
-                            findsearch.push(array2[i].value.firstname)
-                        } else{
-                           // console.log("nix gefunden");
-                        }   
-                    });
-                }
-            };
-
-            */
-
-            // findsearchUnique =[...new Set(findsearch)]
-            console.log(userbook.searchtheme,"--thema"); 
+            alleEintraege.forEach(ele1 => {
+                // console.log(ele1.value.books);
+                console.log(ele1.value.books[0]);
+                for(let i=0; i < ele1.value.books.length; i++) {
+                    console.log(ele1.value.books[i],(ele1.value.firstname + "username"));
+                     
+                }     
+            })    
+            
+             
             res.status(200).send({
                 findsearchBack:   findsearch,
                 searchthemeBack:  userbook.searchtheme,
