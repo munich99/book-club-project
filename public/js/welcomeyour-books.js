09cbdfdc17ed56b9685d1d2954dbc0e8bffafabd
munich99 <regerstrasse@icloud.com>
2020-02-17 21:47:38 +0100

    list will change to table

--- public/js/welcomeyour-books.js
+++ public/js/welcomeyour-books.js
@@ -3,24 +3,41 @@
 
 document.addEventListener ( 'DOMContentLoaded', () => {	// DOM-Elemente 
 
-    console.log(userGesamtIngesamt,"xx");
     document.getElementById("willkommen").innerHTML = userGesamtIngesamt.value.firstname;
 
-    let textnode, node, newElement, newTextnode, addmeaning = ["", ", ", " gelesen, Genre: ", ""]; 	
+    let textnodeTitle, node, newElementTitle, textnodeTitleZusatz, textnodeAuthor, newTextnode, addmeaning = ["", ", ", " gelesen, Genre: ", ""]; 	
 	
 	if(userGesamtIngesamt.value.books){
 		userGesamtIngesamt.value.books.forEach(key => {	
+            console.log();
+            
 
-			node 	 = document.createElement("li");		
-			textnode = document.createTextNode(Object.values(key)[0] + " von "); 
-			node.appendChild(textnode);		
+            node 	 = document.createElement("li");
+            
+            newElementTitle  = document.createElement("span");
+            newElementTitle.className = "title";            
+            textnodeTitle = document.createTextNode(key.title);
+            newElementTitle.appendChild(textnodeTitle);   
+            //textnodeAuthor = document.createTextNode(key.author); 
+            console.log(key);
+            
+
+            textnodeTitleZusatz = document.createTextNode(" von ");
+            textnodeAuthor = document.createTextNode(key.author);            
+            
+            
+            node.appendChild(newElementTitle);
+            node.appendChild(textnodeTitleZusatz);
+            node.appendChild(textnodeAuthor);
+
+            // node.appendChild(textnodeAuthor);		
 			
-			for(let i=1; i<=3; i++){			
-				newElement = document.createElement("span");
+			/* for(let i=1; i<=3; i++){			
+				newElement  = document.createElement("span");
 				newTextnode = document.createTextNode( Object.values(key)[i] + addmeaning[i] );
 				newElement.appendChild(newTextnode);
 				node.appendChild(newElement)
-			}
+			} */
 
 			document.getElementById("deine-buecher-liste").appendChild(node);	
 
